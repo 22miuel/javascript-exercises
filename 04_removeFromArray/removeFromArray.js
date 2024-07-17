cc77da5baeebf9660624c78fdbf50f73f8a52f7f
22miuel <22miguel.gallardo@gmail.com>
2024-07-17 10:30:04 -0400

    feat: new exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function() {
-};
+    if (!args.includes(item)) {
+        newArray.push(item);
+}
+    };
+    return newArray;
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function() {
-
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
